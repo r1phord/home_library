e537e6290c1e8fbeda4a475877edf1cf0ae98800
r1phord <vlad.gorenkov.1996@mail.ru>
2020-02-10 13:34:21 +0200

    changed tabs to spaces

--- book.py
+++ book.py
@@ -1,4 +1,4 @@
 class Book:
-	def __init__(self, title, author_name):
-		self.title = title
-		self.author_name = author_name
+    def __init__(self, title, author_name):
+        self.title = title
+        self.author_name = author_name
